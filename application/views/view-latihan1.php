e2b6ade0973545d878265e162bf2bda30936aa1f
Atsarjs12 <atsarjuhaedi12@gmail.com>
2023-10-08 18:22:08 +0700

    first commit

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <titile>Latihan 1</titile>
+
+</head>
+
+<body>
+    Hallo Kawan.. Yuk kita belajar web proggraming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
